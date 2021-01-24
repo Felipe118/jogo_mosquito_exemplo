1517c416b493b7dd9c76d93d4cba219afd96c65d
Luis Felipe <lipebezerraa@gmail.com>
2021-01-24 16:12:55 -0300

    Modificações Do jogo e finalização

--- jogo.js
+++ jogo.js
@@ -4,6 +4,22 @@ let largura = 0
 let vidas = 1
 let tempo = 10
 
+var criaMosquitoTempo = 1500
+
+let nivel = window.location.search
+nivel = nivel.replace('?','')
+
+if(nivel === 'normal'){
+    criaMosquitoTempo = 1500
+
+}else if(nivel === 'dificil'){
+    criaMosquitoTempo = 1000
+
+}else if(nivel === 'hard'){
+    criaMosquitoTempo = 700
+
+}
+
 function ajustaTamanhoPalcoJogo(){
 altura = window.innerHeight
  largura = window.innerWidth
